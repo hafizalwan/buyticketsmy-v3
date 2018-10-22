71cace10b18bc69e9b03a9dee4150cd7715a080a
Hafiz Alwan <hafizalwan@gmail.com>
2018-10-23 01:47:08 +0800

    Modified controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :fullname, presence: true, length: {maximum: 20}
+
+  has_many :activities
+  has_many :reservations
+
 end
