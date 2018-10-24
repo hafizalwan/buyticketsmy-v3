c1fb3290cf9ef29856635b37004cf41a1ec80fb9
Hafiz Alwan <hafizalwan@gmail.com>
2018-10-24 19:15:03 +0800

    Removed all review related

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,10 +1,4 @@
 class Activity < ApplicationRecord
   belongs_to :user
-
   has_many :reservations
-  has_many :guest_reviews
-
-  def average_rating
-    guest_reviews.count == 0 ? 0 : guest_reviews.average(:star).round(2).to_i
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,5 @@ class User < ApplicationRecord
 
   has_many :activities
   has_many :reservations
-  has_many :guest_reviews, class_name: "GuestReview", foreign_key: "user_id"
 
 end
