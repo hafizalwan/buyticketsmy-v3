38409fd40b452faa3adff000e78624038dbf6a46
Hafiz Alwan <hafizalwan@gmail.com>
2018-10-27 14:10:37 +0800

    Added active_storage has_many_attached

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,7 @@
 class Activity < ApplicationRecord
   belongs_to :user
   has_many :reservations
+  has_many_attached :photos
 
   geocoded_by :location_map
   after_validation :geocode, if: :location_map_changed?
