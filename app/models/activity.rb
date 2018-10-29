f1f2b537022d817a91e528fd9f6d22932eccf7ce
Hafiz Alwan <hafizalwan@gmail.com>
2018-10-30 01:58:17 +0800

    Changed has_many photos to has_many images

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,7 @@
 class Activity < ApplicationRecord
   belongs_to :user
   has_many :reservations
-  has_many_attached :photos
+  has_many_attached :images
 
   geocoded_by :location_map
   after_validation :geocode, if: :location_map_changed?
