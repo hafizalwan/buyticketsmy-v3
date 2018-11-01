8e4ee7e56504d4d98b47f1897db82a48ba34b419
Hafiz Alwan <hafizalwan@gmail.com>
2018-11-01 19:34:43 +0800

    Deleted create and get bill from Billplz and added new success action

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -10,34 +10,24 @@ class ReservationsController < ApplicationController
       @reservation = current_user.reservations.build(reservation_params)
       @reservation.activity = activity
       @reservation.total = (activity.my_adult_price * @reservation.my_adult_count) + (activity.my_child_price * @reservation.my_child_count) + (activity.int_adult_price * @reservation.int_adult_count) + (activity.int_child_price * @reservation.int_child_count)
-      bill = Billplz::Bill.new(
-        collection_id: "qdnzbjqi",
-        email: "current_user.email",
-        name: "current_user.fullname",
-        amount: "reservation.total * 100",
-        callback_url: "http://localhost:3000/webhook",
-        description: "activity.name"
-      )
 
       if @reservation.save
+        redirect_to reservation_success_path
         flash[:notice] = "Reservation created successfully!"
       else
         flash[:alert] = "Something is wrong, please try again!"
       end
-      redirect_to activity
     end
   end
 
+  def success
+  end
 
   def your_activities
     @bookings = current_user.reservations.order(arrival: :asc)
   end
 
   private
-
-    def charge(activity, reservation)
-
-    end
 
     def reservation_params
       params.require(:reservation).permit(:arrival, :my_adult_count, :my_child_count, :int_adult_count, :int_child_count)
