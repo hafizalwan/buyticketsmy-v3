a67f5efcc0dfefee567e1c48c7d7a1f087f96f5e
Hafiz Alwan <hafizalwan@gmail.com>
2018-11-05 01:58:42 +0800

    Added admin and host columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_03_093303) do
+ActiveRecord::Schema.define(version: 2018_11_04_175252) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 2018_11_03_093303) do
     t.datetime "updated_at", null: false
     t.string "fullname"
     t.integer "phone_number"
+    t.boolean "is_admin", default: false
+    t.boolean "is_host", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
