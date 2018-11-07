90ef9c98788e2caeb484819ffd159a83d88ae2f4
Hafiz Alwan <hafizalwan@gmail.com>
2018-11-07 15:15:14 +0800

    Scope for current week revenue

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,10 @@ class Reservation < ApplicationRecord
 
   belongs_to :user
   belongs_to :activity
+
+  scope :current_week_revenue, -> (user) {
+    joins(:activity)
+    .where('activities.user_id = ? AND reservations.updated_at = ? AND reservations.status = ?', user.id, 1.week.ago, 1)
+    .order(updated_at: :asc)
+  }
 end
