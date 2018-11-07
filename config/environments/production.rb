6b00aa7f2fbbda1fa7a3e1d6867bac29782a9243
Hafiz Alwan <hafizalwan@gmail.com>
2018-11-07 17:50:46 +0800

    Updated credentials to use encypted credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,8 +42,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'buytickets.my' }
 
   ActionMailer::Base.smtp_settings = {
-  :user_name => "<%= ENV['SENDGRID_USERNAME'] %>",
-  :password => "<%= ENV['SENDGRID_PASSWORD'] %>",
+  :user_name => "<%= Rails.application.credentials.dig(:sendgrid, :sendgrid_username) %>",
+  :password => "<%= Rails.application.credentials.dig(:sendgrid, :sendgrid_password) %>",
   :domain => "buytickets.my",
   :address => "smtp.sendgrid.net",
   :port => 587,
