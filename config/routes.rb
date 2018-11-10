d34ff682ab3afe9f3463f570165e55cebfb649e7
Hafiz Alwan <hafizalwan@gmail.com>
2018-11-11 00:13:29 +0800

    Modified devise's routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration', },
-  controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+  controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
 
   resources :activities, except: [:edit] do
     member do
