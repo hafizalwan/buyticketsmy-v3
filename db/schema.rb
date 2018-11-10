56d9f4947d474ac9512be9875d4a72c9b951fe88
Hafiz Alwan <hafizalwan@gmail.com>
2018-11-11 00:15:56 +0800

    Change phone_number datatype to bigint(limit 8 bytes)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_185930) do
+ActiveRecord::Schema.define(version: 2018_11_10_154600) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -85,7 +85,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_185930) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "fullname"
-    t.integer "phone_number"
+    t.bigint "phone_number"
     t.boolean "is_admin", default: false
     t.string "confirmation_token"
     t.datetime "confirmed_at"
