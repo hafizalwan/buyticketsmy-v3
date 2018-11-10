157f40c6613f0a8b7ec98e8b333e72a96e3aaaa2
Hafiz Alwan <hafizalwan@gmail.com>
2018-11-10 20:06:32 +0800

    Changed daily date views in revenue chart

--- app/controllers/revenues_controller.rb
+++ app/controllers/revenues_controller.rb
@@ -7,7 +7,7 @@ class RevenuesController < ApplicationController
     @this_week_revenue = @reservations.map {|r| {r.updated_at.strftime('%Y-%m-%d') => r.total} }
                                       .inject({}) {|a,b| a.merge(b){|_,x,y| x + y}}
 
-    @this_week_revenue = Date.today().all_week.map { |date| [date.strftime('%a'), @this_week_revenue[date.strftime('%Y-%m-%d')] || 0 ]}
+    @this_week_revenue = Date.today().all_week.map { |date| [date.strftime('%D'), @this_week_revenue[date.strftime('%Y-%m-%d')] || 0 ]}
   end
 
 end
