866ea4423dbaecce56beb39c0ff21baba8e5616e
Hafiz Alwan <hafizalwan@gmail.com>
2018-11-12 22:19:39 +0800

    Change user to guest

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,6 +1,6 @@
 class ReservationMailer < ApplicationMailer
-  def send_email_to_guest(user, activity)
-    @recipient = user
+  def send_email_to_guest(guest, activity)
+    @recipient = guest
     @activity = activity
     mail(to: @recipient.email, subject: 'Your reservation is confirmed!')
   end
