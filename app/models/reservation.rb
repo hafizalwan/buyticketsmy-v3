444dccd0d09b96ecc3cb5525c6d21c061adb1676
Hafiz Alwan <hafizalwan@gmail.com>
2018-11-17 21:57:27 +0800

    Added receipt method for receipt creation(untested)

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,4 +9,25 @@ class Reservation < ApplicationRecord
     .where('activities.user_id = ? AND reservations.updated_at >= ? AND reservations.status = ?', user.id, 1.week.ago, 1)
     .order(updated_at: :asc)
   }
+
+  def receipt
+    Receipts::Receipt.new(
+      id: id,
+      subheading: "Receipt for #{activity.name}",
+      product: "#{activity.name}",
+      company: {
+        name: "BTO Marketing",
+        address: "No.1, Bangunan MPLBP\nTeluk Yu\nLangkawi, Kedah 07000",
+        email: "buyticketsmarketing@gmail.com",
+        logo: Rails.root.join("app/assets/images/receiptlogo/main-logo.png")
+      },
+      line_items: [
+        ["Arrival Date",   arrival.strftime("%e %b %Y")],
+        ["Account Billed", "#{user.fullname} (#{user.email})"],
+        ["Product",        "#{activity.name}"],
+        ["Amount",         "MYR#{total}"],
+      ]
+    )
+  end
+
 end
