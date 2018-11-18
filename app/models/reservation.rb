28512300da8d70d345efe2b6e42b8e4e1e000050
Hafiz Alwan <hafizalwan@gmail.com>
2018-11-19 04:32:20 +0800

    Removed unused method

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -10,24 +10,4 @@ class Reservation < ApplicationRecord
     .order(updated_at: :asc)
   }
 
-  def receipt
-    Receipts::Receipt.new(
-      id: id,
-      subheading: "Receipt for #{activity.name}",
-      product: "#{activity.name}",
-      company: {
-        name: "BTO Marketing",
-        address: "No.1, Bangunan MPLBP\nTeluk Yu\nLangkawi, Kedah 07000",
-        email: "buyticketsmarketing@gmail.com",
-        logo: Rails.root.join("app/assets/images/receiptlogo/main-logo.png")
-      },
-      line_items: [
-        ["Arrival Date",   arrival.strftime("%e %b %Y")],
-        ["Account Billed", "#{user.fullname} (#{user.email})"],
-        ["Product",        "#{activity.name}"],
-        ["Amount",         "MYR#{total}"],
-      ]
-    )
-  end
-
 end
