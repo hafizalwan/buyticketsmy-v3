2e444bd93f6cc07a65774652ec964290d24e5abc
Hafiz Alwan <hafizalwan@gmail.com>
2018-11-29 05:11:02 +0800

    Removed default comments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,29 +6,16 @@ Rails.application.configure do
   config.require_master_key = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
   config.assets.compile = false
   config.active_storage.service = :amazon
-  # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.force_ssl = true
   config.log_level = :debug
   config.log_tags = [ :request_id ]
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-  # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "buyticketsmy-v3_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
-  # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
@@ -36,7 +23,6 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'buytickets.my' }
